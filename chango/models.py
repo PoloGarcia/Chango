ee4f9246efc2707bf7ae0b4a1eba62f15d77c599
Noe Loyola <noeloyolag@gmail.com>
2015-02-05 15:12:41 -0600

    Fixed model name

--- chango/models.py
+++ chango/models.py
@@ -2,10 +2,12 @@ from django.db import models
 from django.utils import timezone
 import datetime
 
-class Poll(models.Model):
-	name = models.CharField(max_length=200)
-	description = models.CharField(max_length=400)
-	geo_location = models.CharField(max_length=50)
+
+class Chango(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=400)
+    geo_location = models.CharField(max_length=50)
     pub_date = models.DateTimeField('date published')
-    def __unicode__(self):  # Python 3: def __str__(self):
+
+    def __unicode__(self): 
         return self.name
